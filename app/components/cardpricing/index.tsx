11dee5cb91d218c7a67f50a9a8c005b3f2a62f7a
Aviel Aldama <aviel.aldama@gmail.com>
2024-09-30 16:17:04 -0600

    style mobile

--- app/components/cardpricing/index.tsx
+++ app/components/cardpricing/index.tsx
@@ -2,7 +2,7 @@ const Index = (props: { section: string, description: string, promos: Array<{ pr
     return (
         <section className="">
             <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
-                <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
+                <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-10 mt-16 sm:mt-0">
                     <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900">{props.section}</h2>
                     <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">{props.description}</p>
 
@@ -16,7 +16,7 @@ const Index = (props: { section: string, description: string, promos: Array<{ pr
                                 <div className="flex items-baseline justify-center my-8">
                                     <span className="mr-2 text-5xl font-extrabold">$ {item.price} mxn</span>
                                 </div>
-                                <ul role="list" className="mb-8 space-y-4 text-left">
+                                <ul role="list" className="mb-4 space-y-4 text-left">
                                     {
                                         item.listbonus.map((item, index) => (
                                             <li key={index} className="flex items-center space-x-3">
@@ -27,7 +27,7 @@ const Index = (props: { section: string, description: string, promos: Array<{ pr
                                         ))
                                     }
                                 </ul>
-                                <a href="#" className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-purple-900">Get started</a>
+                                <a href="#" className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-purple-900">mas Información</a>
                             </div>
 
                         ))
--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,6 +1,4 @@
 'use client'
-import Image from "next/image"
-import Logo from '@/app/assets/logo.jpeg'
 import { useState } from "react"
 import Link from "next/link"
 
@@ -11,19 +9,14 @@ const Index = (props: { options: Array<{ ruta: string, uri: string }> }) => {
             <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
                 <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
                     <a href="" className="flex items-center">
-                        <Image src={Logo} className="h-6 mr-3 sm:h-9 w-10" alt="Image logo"></Image>
-                        <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white font-inspiration">LocalFix</span>
+                        <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white font-inspiration">LocalFix</span>
                     </a>
                     <div className="flex items-center lg:order-2">
-                        <div className="hidden mt-2 mr-4 sm:inline-block">
-                            <a href="">Start</a>
-                        </div>
-                        <a href="" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Download</a>
                         <button
                             onClick={() => setOpenMenu(!openMenu)}
                             className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                             <span className="sr-only">Open main menu</span>
-                            <i className="bi bi-list"></i>
+                            <i className="bi bi-list text-2xl"></i>
                         </button>
                     </div>
                     <div className={`${openMenu ? 'visible' : 'hidden'} items-center justify-between w-full lg:flex lg:w-auto lg:order-1`}>
