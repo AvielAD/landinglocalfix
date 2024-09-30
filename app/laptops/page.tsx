96a587990e25eb572a4e8d417302af70633bead4
Aviel Aldama <aviel.aldama@gmail.com>
2024-09-30 16:37:09 -0600

    ortografia

--- app/laptops/page.tsx
+++ app/laptops/page.tsx
@@ -13,44 +13,44 @@ export default Index
 
 const bannerpromo = {
     section: "Actualización para Laptops",
-    description: "Presentamos un conjunto de precios , sobre la actualizacion de laptops o pc para mejorar la velocidad, estos paquetes de actualización son validos para el SSD o mejor conocido como disco duro."
+    description: "Presentamos un conjunto de precios, sobre la actualizacion de laptops o PC Escritorio para mejorar la velocidad, estos paquetes de actualización son válidos para el disco duro interno (HDD/SSD) 3.5 ."
 }
 
 const promos = [
     {
         product: "SSD 120",
-        description: "Espacio optimo para trabajo de ofimatica, programas basicos , no se recomienda almacenamiento multimedia",
+        description: "Espacio optimo para trabajo de ofimatica, programas básicos , no se recomienda almacenamiento multimedia",
         price: 700,
         listbonus: [ 
             "Limpieza interna hardware",
             "Cambio pasta termica",
-            "Instalacion sistema operativo",
-            "Configuracion inicial orientada al rendimiento",
-            "Garantia directa 3 meses"
+            "Instalación sistema operativo",
+            "Configuración inicial orientada al rendimiento",
+            "Garantía directa 3 meses"
         ]
     },
     {
         product: "SSD 240",
-        description: "Espacio optimo para escuela, programas basicos, almacenamiento moderado de archivos",
+        description: "Espacio optimo para escuela, programas básicos, almacenamiento moderado de archivos",
         price: 900,
         listbonus: [ 
             "Limpieza interna hardware",
             "Cambio pasta termica",
-            "Instalacion sistema operativo",
-            "Configuracion inicial orientada al rendimiento",
-            "Garantia directa 3 meses"
+            "Instalación sistema operativo",
+            "Configuración inicial orientada al rendimiento",
+            "Garantía directa 3 meses"
         ]
     },
     {
         product: "SSD 480",
-        description: "Espacio optimo para escuela, programas basicos, almacenamiento moderado de archivos y almacenamiento multimedia",
+        description: "Espacio optimo para escuela, programas básicos, almacenamiento moderado de archivos y multimedia",
         price: 1200,
         listbonus: [ 
             "Limpieza interna hardware",
             "Cambio pasta termica",
-            "Instalacion sistema operativo",
-            "Configuracion inicial orientada al rendimiento",
-            "Garantia directa 3 meses"
+            "Instalación sistema operativo",
+            "Configuración inicial orientada al rendimiento",
+            "Garantía directa 3 meses"
         ]
     }
 ]
