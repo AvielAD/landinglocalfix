5423011b8a1d86ec4d1eeadcca9cb9d124f8a91c
Aviel Aldama <aviel.aldama@gmail.com>
2024-09-30 16:39:29 -0600

    ortografia

--- app/components/topsection/index.tsx
+++ app/components/topsection/index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl">
                         Reparación de celulares<br /> y computadoras</h1>
-                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Asesoria en reparacion de dispositivos electronicos</p>
+                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Asesoría en reparación de dispositivos electrónicos</p>
                     <div></div>
                 </div>
                 <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
